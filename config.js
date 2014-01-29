1a27ea53be2c0f9c20510dd54fee21625defe39f
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-01-29 14:51:22 +0000

    removed sensitive data

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var ids = {
 	facebook: {
-		clientID: '439773292818484',
-		clientSecret: '9a9eaa46b7f8b7b1c635cba4b9f0efd5',
+		clientID: '',
+		clientSecret: '',
 		callbackURL: 'http://riddlr.jit.su/redirect',
 		callbackURLLocal : 'http://localhost:3000/redirect',
-		appAccessToken: '439773292818484|iq6L1zJgn-ZFHO_61c1Ocf0uL6c',
+		appAccessToken: '',
 		verifyToken : 'wearetherookiechamps'
 	}
 }
